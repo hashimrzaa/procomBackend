3415e518db99eb82481be828269ebe6c595de332
hashimrzaa <hashimterminator20@gmail.com>
2024-03-12 06:59:33 +0500

    update

--- routes/index.mjs
+++ routes/index.mjs
@@ -1,11 +1,9 @@
 import express from "express";
 import users from "./user.mjs";
-import orders from "./order.mjs";
 import products from "./products.mjs";
 const router = express.Router();
 
 router.use("/users", users);
-router.use("/orders", orders);
 router.use("/products", products);
 
 export default router;
